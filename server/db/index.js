ef5f74ad5c8326bdb09a69ef6c6077e98920453d
joker-jonesy <spashmcgrath94@yahoo.com>
2023-05-11 09:24:01 -0400

    shortened index.js in db

--- server/db/index.js
+++ server/db/index.js
@@ -2,18 +2,8 @@
 // with your models, for which you'll find some blank files in this directory:
 
 const db = require('./database')
-const Student = require('./student')
-const Campus = require('./campus')
-
-// This is a great place to establish associations between your models
-// (https://sequelize-guides.netlify.com/association-types/).
-// Example:
-//
-// Puppy.belongsTo(Owner)
 
 module.exports = {
     // Include your models in this exports object as well!
     db,
-    Student,
-    Campus,
 }
